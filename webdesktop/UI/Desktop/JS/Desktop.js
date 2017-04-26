09dc9893e7438927dc20590d7138f20edc2cecc4
WangJun <wangjun19850215@live.cn>
2017-04-26 17:19:01 +0800

    添加部分桌面代码
    添加部分桌面代码

--- webdesktop/UI/Desktop/JS/Desktop.js
+++ webdesktop/UI/Desktop/JS/Desktop.js
@@ -1,8 +1,7 @@
-﻿/// <reference path="E:\WangJunCode\WangJun\WangJun\WebDesktop\JS/Base/jquery-3.1.1.min.js" />
-/// <reference path="E:\WangJunCode\WangJun\WangJun\WebDesktop\JS/Base/jquery-ui-1.12.1/jquery-ui.js" />
+﻿
 
 $(document).ready(function () {
-    $('.desktop_dialog').draggable();
+    DESKTOP.Initial();
 });
 
 var DESKTOP = {
@@ -11,10 +10,26 @@ var DESKTOP = {
 
 ///创建一个对话框
 DESKTOP.CreateDialog = function () {
-    var id=
-    var html ='<div class="desktop_dialog" id="test">'
-                +'<div class="title"><a href="javascript:void(0)">浏览器</a><a href="javascript:void(0)">最小化</a><a href="javascript:void(0)">最大化</a><a href="javascript:void(0)" onclick="$(this).parents('.desktop_dialog').remove()">关闭</a></div>'
-                +'<iframe style="border:none; width:100%;height:90%;" src="http://www.toutiao.com"></iframe>'
-                +'</div>';
-    $()
+    var id = "test";
+    var url ="http://www.toutiao.com";
+    var html ='<div class="desktop_dialog" id="'+id+'">'
+        + '<div class="title"><a href="javascript:void(0)" class="name">浏览器</a><a href="javascript:void(0)" class="button" onclick="$(this).parents(\'.desktop_dialog\').remove()">关闭</a><a href="javascript:void(0)" class="button">最大化</a><a href="javascript:void(0)" class="button">最小化</a></div>'
+            +'<iframe style="border:none; width:100%;height:90%;" src="'+url+'"></iframe>'
+            +'</div>';
+    $(document.body).append($(html));
+    $('.desktop_dialog').draggable();
+}
+
+///方法绑定
+DESKTOP.EventBind = function () {
+    $("[data-method]").each(function () {
+        var val = $(this).attr("data-method");
+        $(this).attr("onclick", val + "()");
+        $(this).removeAttr("data-method");
+    });
+}
+
+///桌面初始化
+DESKTOP.Initial = function () {
+    DESKTOP.EventBind();
 }
